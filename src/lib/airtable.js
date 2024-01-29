3c9b9a816965d756236e6fe7fa8cc7c46cd52f5c
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-01-29 13:55:56 -0500

    update airtable auth token

--- src/lib/airtable.js
+++ src/lib/airtable.js
@@ -1,14 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
-const TABLE_ID = `appMPDyS7z5C6dIiS`
+const TABLE_ID = `appMPDyS7z5C6dIiS`;
 
 export const airtable = axios.create({
   baseURL:
-    process.env.NODE_ENV === 'development'
+    process.env.NODE_ENV === "development"
       ? `https://api.airtable.com/v0/${TABLE_ID}`
       : `https://hc-caching-proxy.herokuapp.com/airtable/${TABLE_ID}`,
   headers:
-    process.env.NODE_ENV === 'development'
-      ? { Authorization: `Bearer key1XrFuYhG1GKHvu` }
+    process.env.NODE_ENV === "development"
+      ? {
+          Authorization: `Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6`,
+        }
       : undefined,
-})
+});
